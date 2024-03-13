b5e4fb81a341286ca7d65bc05b8c1c71787c46a0
Jeong Geol <cnowse@gmail.com>
2024-03-13 22:29:30 +0800

    统一结果返回

--- cloud-provider-payment8001/src/main/java/cn/cnowse/cloud/controller/PayController.java
+++ cloud-provider-payment8001/src/main/java/cn/cnowse/cloud/controller/PayController.java
@@ -1,12 +1,7 @@
 package cn.cnowse.cloud.controller;
 
-import cn.cnowse.cloud.entities.Pay;
-import cn.cnowse.cloud.entities.PayDTO;
-import cn.cnowse.cloud.service.PayService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +12,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import cn.cnowse.cloud.entities.Pay;
+import cn.cnowse.cloud.entities.PayDTO;
+import cn.cnowse.cloud.resp.ResultData;
+import cn.cnowse.cloud.service.PayService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author Jeong Geol 2024-3-3
@@ -26,47 +28,47 @@ import java.util.List;
 @RestController
 @RequestMapping("/pay")
 @RequiredArgsConstructor
-@Tag(name = "支付微服务模块",description = "支付CRUD")
+@Tag(name = "支付微服务模块", description = "支付CRUD")
 public class PayController {
 
     private final PayService payService;
 
     @PostMapping("/add")
-    @Operation(summary = "新增",description = "新增支付流水方法,json串做参数")
-    public String addPay(@RequestBody Pay pay) {
+    @Operation(summary = "新增", description = "新增支付流水方法,json串做参数")
+    public ResultData<String> addPay(@RequestBody Pay pay) {
         log.info(pay.toString());
         int result = payService.add(pay);
-        return "成功插入：" + result;
+        return ResultData.success("成功插入数据：" + result);
     }
 
     @DeleteMapping("/del/{id}")
-    @Operation(summary = "删除",description = "删除支付流水方法")
-    public Integer delPay(@PathVariable("id") Integer id) {
-        return payService.delete(id);
+    @Operation(summary = "删除", description = "删除支付流水方法")
+    public ResultData<Integer> delPay(@PathVariable("id") Integer id) {
+        return ResultData.success(payService.delete(id));
     }
 
     @PutMapping("/update")
-    @Operation(summary = "修改",description = "修改支付流水方法")
-    public String updatePay(@RequestBody PayDTO dto) {
+    @Operation(summary = "修改", description = "修改支付流水方法")
+    public ResultData<String> updatePay(@RequestBody PayDTO dto) {
         Pay pay = new Pay();
         BeanUtils.copyProperties(dto, pay);
         int update = payService.update(pay);
-        return "修改成功：" + update;
+        return ResultData.success("修改成功：" + update);
     }
 
     @GetMapping("/{id}")
-    @Operation(summary = "按照ID查流水",description = "查询支付流水方法")
-    public Pay getById(@PathVariable("id") Integer id) {
-        return payService.getById(id);
+    @Operation(summary = "按照ID查流水", description = "查询支付流水方法")
+    public ResultData<Pay> getById(@PathVariable("id") Integer id) {
+        return ResultData.success(payService.getById(id));
     }
 
     @GetMapping("/list")
-    public List<PayDTO> list() {
-        return payService.list().stream().map(pay -> {
+    public ResultData<List<PayDTO>> list() {
+        return ResultData.success(payService.list().stream().map(pay -> {
             PayDTO payDTO = new PayDTO();
             BeanUtils.copyProperties(pay, payDTO);
             return payDTO;
-        }).toList();
+        }).toList());
     }
 
 }
